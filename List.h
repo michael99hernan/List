7c21b4db6aad6b95a9fdab3f914a8e51b482cec6
michael99hernan <55895586+michael99hernan@users.noreply.github.com>
2019-10-29 18:27:57 -0500

    Added comments

--- List.h
+++ List.h
@@ -1,3 +1,7 @@
+//Michael Hernandez
+//CS3377
+//Assignment 9
+//This demonstates a template for link list
 #ifndef LIST_H
 #define LIST_H
 #include <iostream>
@@ -6,19 +10,19 @@ template <class T>
 struct Node
 {
 	T data;
-	Node<T> * next;
+	Node<T> * next; //Pointer to next node
 };
 template <class T>
 class List
 {
 public:
 	List<T>();
-	Node<T> * head;
-	void append(T,Node<T>*&);
-	void display(Node<T>*&);
-	void insert(T);
-	bool deleteNode(T);
-	~List<T>();
+	Node<T> * head;//Head to list
+	void append(T,Node<T>*&);//Create List recursivly
+	void display(Node<T>*&);//Display list recursivly
+	void insert(T); //Inserting a node in the right order
+	bool deleteNode(T); //Remove Node
+	~List<T>(); //Deletes head
 };
 
 #endif
--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,7 @@
+//Michael Hernandez
+//CS3377
+//Assignment 9
+//Source file for project and creates a list with int and strings
 #include "List.h"
 #include "List.cpp"// This was needed to fix a linker issue
 #include <string>
@@ -6,17 +10,17 @@ int main()
 {
 	List<int> mylist;
 
-
+	//Create list of ints
 	mylist.append(50, mylist.head);
 	mylist.append(60, mylist.head);
 	mylist.append(70, mylist.head);
-
+	//Insert 5 nodes
 	mylist.insert(65);
 	mylist.insert(200);
 	mylist.insert(750);
 	mylist.insert(300);
 	mylist.insert(25);
-
+	//Display and delete
 	mylist.display(mylist.head);
 	mylist.deleteNode(50);
 	mylist.display(mylist.head);
@@ -24,21 +28,20 @@ int main()
 	mylist.display(mylist.head);
 	//Part Two
 	List<string> myStringlist;
-
+	//Create list of string
 	myStringlist.append("Michael", myStringlist.head);
 	myStringlist.append("Morgan", myStringlist.head);
 	myStringlist.append("Pat", myStringlist.head);
 	myStringlist.append("Sally", myStringlist.head);
-
+	//Insert 5 nodes
 	myStringlist.insert("Alice");
 	myStringlist.insert("Zach");
 	myStringlist.insert("Omer");
 	myStringlist.insert("Blake");
 	myStringlist.insert("Jim");
-
+	//Display and delete
 	myStringlist.display(myStringlist.head);
 	myStringlist.deleteNode("Zach");
-
 	myStringlist.display(myStringlist.head);
 	myStringlist.deleteNode("Morgan");
 	myStringlist.display(myStringlist.head);
